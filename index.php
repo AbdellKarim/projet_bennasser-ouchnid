d6493f090eaa5f05c077dc37ae390de83d598acc
abdelilah <ab68442879@gmail.com>
2024-11-20 16:33:07 +0100

    ADD OOUCHNID

--- index.php
+++ index.php
@@ -15,14 +15,14 @@
       <section>
       <img src="\projet_bennasser-ouchnid\photo\urca.png" alt="Description de l'image">
         <h2>Projet </h2>
-
+        
         <p><a href="Client\Client.html">Client</a></p>
         <p><a href="Produit\Produit.html">Produit</a></p>
         <p><a href="Commande\Commande.html">Commande</a></p>
         <p><a href="serveur1.php"></a></p>    
         <p> <a href="info.php">info.php</a></p>
         <p>  <a href="navigateur.php">navigateur.php</a></p>
-        <p>amine</p>
+        
       </section>
     </main>
     <footer>
