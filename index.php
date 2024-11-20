e596e6e6cd9a90620af7d0861c3531ebc4571d4b
abdelkarim ben nasser <ab68442879@gmail.com>
2024-11-20 08:58:54 +0100

    version2

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Pages Web en PHP</title>
+  </head>
+  <body>
+    <header>
+      <h1><a href="index.php">Projet II0902 : Systèmes d'information embarqués</a></h1>
+    </header>
+    <nav>
+      <!-- Insérer une barre de navigation ici -->
+    </nav>
+    <main>
+      <section>
+        <h2>Projet </h2>
 
-<?php
-phpinfo();
-?>
+        <p><a href="index.php">Client</a></p>
+        <p><a href="index.html">Produit</a></p>
+        <p><a href="tableau01.php">Commande</a></p>
+        <p><a href="serveur1.php"></a></p>    
+        <p> <a href="info.php">info.php</a></p>
+        <p>  <a href="navigateur.php">navigateur.php</a></p>
+      </section>
+    </main>
+    <footer>
+      <!-- Insérer un pied de page ici -->
+    </footer>
+  </body>
+</html>
