b6b91396941645e1b0aaa742e0b6715428f44175
abdelkarim ben nasser <ab68442879@gmail.com>
2024-11-20 09:24:11 +0100

    creation Repertoire

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
       <img src="\projet_bennasser-ouchnid\photo\urca.png" alt="Description de l'image">
         <h2>Projet </h2>
 
-        <p><a href="index.php">Client</a></p>
-        <p><a href="index.html">Produit</a></p>
-        <p><a href="tableau01.php">Commande</a></p>
+        <p><a href="Client\Client.html">Client</a></p>
+        <p><a href="Produit\Produit.html">Produit</a></p>
+        <p><a href="Commande\Commande.html">Commande</a></p>
         <p><a href="serveur1.php"></a></p>    
         <p> <a href="info.php">info.php</a></p>
         <p>  <a href="navigateur.php">navigateur.php</a></p>
