8dfe4ae7d3bddc0bb46a4397c05f7f289a6a0505
abdelkarim ben nasser <ab68442879@gmail.com>
2024-11-20 18:55:57 +0100

    la barre de menu

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <?php require 'header.php'?>
     <main>
       <section>
-      <img src="\projet_bennasser-ouchnid\photo\urca.png" alt="Description de l'image">
-        <h2>Projet </h2>
+    
+     
+     <h2>Projet </h2>
 
         <p><a href="Client\Client.html">Client</a></p>
         <p><a href="Produit\Produit.html">Produit</a></p>
